efdef63b31d31bc6aece09b99c57539aaf3519c5
Young <youngxdev@gmail.com>
2024-04-05 21:45:12 +0900

    Add cors and set port according to node env

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,24 @@
-import "dotenv/config";
-import { NestFactory, Reflector } from "@nestjs/core";
-import { AppModule } from "./app.module";
-import { ClassSerializerInterceptor, ValidationPipe } from "@nestjs/common";
-import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
-import * as cookieParser from "cookie-parser";
+import 'dotenv/config';
+import { NestFactory, Reflector } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // set CORS
+  app.enableCors();
   // set swagger module
   const config = new DocumentBuilder()
-    .setTitle("Meta Test API")
-    .setDescription("The Meta Test API")
-    .setVersion("1.0.0")
+    .setTitle('Meta Test API')
+    .setDescription('The Meta Test API')
+    .setVersion('1.0.0')
     .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup("api", app, document);
+  SwaggerModule.setup('api', app, document);
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -27,6 +29,13 @@ async function bootstrap() {
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
   app.use(cookieParser());
 
-  await app.listen(3000);
+  // set port
+  const port =
+    process.env.NODE_ENV === 'dev'
+      ? 8000
+      : process.env.NODE_ENV === 'production'
+        ? 8080
+        : 3000;
+  await app.listen(port);
 }
 bootstrap();
