61ef8db54d12c2660d53b1d9e5a9990aad5494e5
Young <youngxdev@gmail.com>
2024-04-20 10:43:29 +0900

    Modify base exception filter

--- src/core/filters/base-exception-filter.ts
+++ src/core/filters/base-exception-filter.ts
@@ -6,25 +6,46 @@ import {
   HttpStatus,
 } from '@nestjs/common';
 import { Response } from 'express';
-import { DatabaseError } from 'src/core/errors/database-error';
 
 @Catch(Error)
 export class BaseExceptionFilter implements ExceptionFilter {
   catch(exception: Error, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
-    const status =
-      exception instanceof HttpException
-        ? exception.getStatus()
-        : HttpStatus.INTERNAL_SERVER_ERROR;
 
-    let message = exception.message ? exception.message : 'Fatal Erorr';
-    let error = 'Internal Server Error';
+    if (exception instanceof HttpException) {
+      const status = exception.getStatus();
+      const exceptionResponse = exception.getResponse();
 
-    response.status(status).json({
-      message,
-      error,
-      statusCode: status,
-    });
+      // Check if the response is an object and has a 'message' property
+      if (
+        typeof exceptionResponse === 'object' &&
+        exceptionResponse !== null &&
+        'message' in exceptionResponse
+      ) {
+        response.status(status).json({
+          message: exceptionResponse['message'],
+          error: exceptionResponse['error'] || exception.name,
+          statusCode: status,
+        });
+      } else {
+        // If the response is not an object or doesn't have a 'message' property, handle as a string or fallback
+        response.status(status).json({
+          message:
+            typeof exceptionResponse === 'string'
+              ? exceptionResponse
+              : 'Unknown error',
+          error: exception.name,
+          statusCode: status,
+        });
+      }
+    } else {
+      // Non-HttpException errors
+      response.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+        message: exception.message,
+        error: 'Internal Server Error',
+        statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+      });
+    }
   }
 }
