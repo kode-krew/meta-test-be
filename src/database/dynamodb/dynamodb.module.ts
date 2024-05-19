e96b3f3de26ab8bf1a86c9f2598728f2956dec36
minsuKang <90169703+minchodang@users.noreply.github.com>
2024-05-19 17:40:19 +0900

    Fix: 환경변수명 수정 (#26)

--- src/database/dynamodb/dynamodb.module.ts
+++ src/database/dynamodb/dynamodb.module.ts
@@ -10,8 +10,8 @@ import { DynamoDBDocument } from "@aws-sdk/lib-dynamodb";
       useFactory: () => {
         const client = new DynamoDB({
           credentials: {
-            accessKeyId: process.env.ACCESS_KEY_ID,
-            secretAccessKey: process.env.SECRET_ACCESS_KEY,
+            accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+            secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
           },
           region: process.env.REGION,
         });
