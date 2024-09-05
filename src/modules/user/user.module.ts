0d8fd3159ea06d3d98221514feaefd3384bc9eb0
minsuKang <90169703+minchodang@users.noreply.github.com>
2024-09-05 22:32:30 +0900

    Fix: supabase 모듈 교체 (#39)

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -2,11 +2,11 @@ import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserRepository } from './user.repository';
 import { UserController } from './user.controller';
-import { DynamoDBModule } from '../../database/dynamodb/dynamodb.module';
 import { AuthModule } from '../auth/auth.module';
+import { SupabaseModule } from 'src/database/supabase/supabase.module';
 
 @Module({
-  imports: [DynamoDBModule, AuthModule],
+  imports: [SupabaseModule, AuthModule],
   controllers: [UserController],
   providers: [UserService, UserRepository],
   exports: [UserService, UserRepository],
