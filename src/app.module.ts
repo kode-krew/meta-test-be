4a08f71f3ff9e3e75b170a46574daf79f9269562
minchodang <minsu910725@gmail.com>
2024-09-06 22:13:32 +0900

    Fix: dynamo db rollback

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { HealthCheckController } from './modules/health-check/health-check.controller';
+import { DynamoDBModule } from './database/dynamodb/dynamodb.module';
 import { UserModule } from './modules/user/user.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { TestModule } from './modules/test/test.module';
-import { SupabaseModule } from './database/supabase/supabase.module';
+
 @Module({
-  imports: [SupabaseModule, UserModule, AuthModule, TestModule],
+  imports: [DynamoDBModule, UserModule, AuthModule, TestModule],
   controllers: [HealthCheckController],
   providers: [],
 })
